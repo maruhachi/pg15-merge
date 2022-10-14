09da71e4ae14d58ca1578fa27e5f14e8a0564cf9
maruhachi <konchan1129@gmail.com>
2022-10-15 04:08:02 +0900

    initial pj. :tada:

---
+++ src/main/java/work/yk0/pg15merge/Pg15MergeApplication.java
@@ -0,0 +1,13 @@
+package work.yk0.pg15merge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pg15MergeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pg15MergeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/work/yk0/pg15merge/Pg15MergeApplicationTests.java
@@ -0,0 +1,13 @@
+package work.yk0.pg15merge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pg15MergeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
